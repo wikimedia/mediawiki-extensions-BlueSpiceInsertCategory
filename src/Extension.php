a2435576d0f75ba189c11207dd9d866b26061b4e
Umherirrender <umherirrender_de.wp@web.de>
2019-04-12 19:29:11 +0200

    Add mediawiki/mediawiki-codesniffer
    Using 25.0.0, because this extension required MediaWiki 1.31
    
    Change-Id: Ic6ed49fbf114e31e5926424bbfefd26b9f85c8ce

--- src/Extension.php
+++ src/Extension.php
@@ -26,7 +26,7 @@
  * @author     Stefan Widmann <widmann@hallowelt.com>
  * @package    BlueSpiceInsertCategory
  * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v3
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
  * @filesource
  */
 
@@ -38,4 +38,5 @@ namespace BlueSpice\InsertCategory;
  * Class for category management assistent
  * @package BlueSpiceInsertCategory
  */
-class Extension extends \BlueSpice\Extension {}
+class Extension extends \BlueSpice\Extension {
+}
