f666645b62f0a1cc9ad0595be1091daa70d5da27
Markus Glaser <glaser@hallowelt.com>
2019-07-11 12:14:59 +0200

    Enable inline editing of categories in new title section
    Needs cherry-pick to master
    
    ERM14955
    
    Change-Id: I849fd7834c711f1e276848cd0015ad77f6f95ce5

--- resources/bluespice.insertCategory.js
+++ resources/bluespice.insertCategory.js
@@ -18,3 +18,48 @@ $(document).on('click', '#ca-insert_category', function(e) {
 	});
 	return false;
 });
+
+$(document).on('click', 'a.bs-category-add-category', function(e) {
+	e.preventDefault();
+	var me = $(this).find('a');
+	me.categoryEditor = null;
+	mw.loader.using( 'ext.bluespice.extjs' ).done(function( me ){
+
+		Ext.require( 'BS.InsertCategory.panel.CategoryInlineEditor', function( me ){
+			//Prevent a second toggle when category field is already visible.
+			//Element id is set in CategoryInlineEditor
+			if ( $( '.bs-insertcategory-category-inline-editor-icon' ).is( ':visible' ) ) {
+				return;
+			}
+
+			$( '.bs-category-add-category' ).hide();
+			$( '.bs-category-container-categories' ).hide();
+
+			if ( me.categoryEditor == null ) {
+				me.categoryEditor = new BS.InsertCategory.panel.CategoryInlineEditor({
+					pageId: mw.config.get( 'wgArticleId' ),
+					allCategories: mw.config.get( 'wgCategories' ),
+					renderTo: 'bs-category-container-add-category'
+				} );
+			} else {
+				me.categoryEditor.show();
+				$( '.bs-insertcategory-category-inline-editor-icon' ).show();
+			}
+
+			me.categoryEditor.btnCancel.on( 'click', function(){
+				me.categoryEditor.hide();
+				$( '.bs-category-add-category' ).show();
+				$( '.bs-category-container-categories' ).show();
+			}, me.categoryEditor );
+
+			$( document ).on( 'click', '.bs-insertcategory-category-inline-editor-icon', function( e ) {
+				me.categoryEditor.hide();
+				$( '.bs-category-add-category' ).show();
+				$( '.bs-category-container-categories' ).show();
+			});
+
+			me.categoryEditor.switchToEditor();
+		}, me );
+	}, me );
+	return false;
+});
