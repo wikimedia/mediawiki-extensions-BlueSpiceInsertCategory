765b889e19e11ed0cc8bafb2a0cd29ece13f6edf
rvogel <vogel@hallowelt.biz>
2019-09-04 16:18:50 +0200

    Remove unused code and resources
    This code was used for `BlueSpiceExtensions/ExtendedEditbar` and
    `BlueSpiceExtensions/VisualEditor` integration.
    
    NEEDS CHERRY-PICK TO `master`!
    
    Change-Id: I0d5cdefd280ed211fe6ef7101bec0a660ee3e5e0

--- resources/bluespice.insertCategory.js
+++ resources/bluespice.insertCategory.js
@@ -1,24 +1,3 @@
-$(document).on('click', '#ca-insert_category', function(e) {
-	e.preventDefault();
-	var me = $(this).find('a');
-	mw.loader.using( 'ext.bluespice.extjs' ).done(function(){
-		Ext.require('BS.InsertCategory.Dialog', function(){
-			BS.InsertCategory.Dialog.clearListeners();
-			BS.InsertCategory.Dialog.on( 'ok', function ( sender, data ) {
-				if ( BS.InsertCategory.Dialog.isDirty ) {
-					BsInsertCategoryViewHelper.setCategories( data );
-					return false;
-				}
-			} );
-			BS.InsertCategory.Dialog.setData(
-				BsInsertCategoryViewHelper.getCategories()
-			);
-			BS.InsertCategory.Dialog.show( me );
-		});
-	});
-	return false;
-});
-
 $(document).on('click', 'a.bs-category-add-category', function(e) {
 	e.preventDefault();
 	var me = $(this).find('a');
