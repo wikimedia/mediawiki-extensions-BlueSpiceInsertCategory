7b4448e03894b39ac5d108a5ac339c7c01c672d7
pwirth <wirth@hallowelt.biz>
2019-12-14 10:45:39 +0100

    Fix doc blocks and line too long for phpcs
    ERM:17071
    [3.1.x]
    
    Change-Id: I436d640ad6c5b94241346df4b849b55a89e51b1d

--- src/ConfigDefinition/UploadPanelIntegration.php
+++ src/ConfigDefinition/UploadPanelIntegration.php
@@ -4,18 +4,31 @@ namespace BlueSpice\InsertCategory\ConfigDefinition;
 
 class UploadPanelIntegration extends \BlueSpice\ConfigDefinition\BooleanSetting {
 
+	/**
+	 *
+	 * @return string[]
+	 */
 	public function getPaths() {
+		$ext = 'BlueSpiceInsertCategory';
 		return [
-			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_CONTENT_STRUCTURING . '/BlueSpiceInsertCategory',
-			static::MAIN_PATH_EXTENSION . '/BlueSpiceInsertCategory/' . static::FEATURE_CONTENT_STRUCTURING,
-			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceInsertCategory',
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_CONTENT_STRUCTURING . "/$ext",
+			static::MAIN_PATH_EXTENSION . "/$ext/" . static::FEATURE_CONTENT_STRUCTURING,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . "/$ext",
 		];
 	}
 
+	/**
+	 *
+	 * @return string
+	 */
 	public function getLabelMessageKey() {
 		return 'bs-insertcategory-pref-uploadpanelintegration';
 	}
 
+	/**
+	 *
+	 * @return bool
+	 */
 	public function isRLConfigVar() {
 		return true;
 	}
