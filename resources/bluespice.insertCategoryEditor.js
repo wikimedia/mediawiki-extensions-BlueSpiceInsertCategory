bbf7324987570904ad9f59a5cf64bb8759888fa5
dvogel <daniel.vogel@hallowelt.com>
2021-08-24 12:50:56 +0200

    Add categories to Mwstake SkinSlot
    [4.x]
    
    ERM25639
    
    Change-Id: I6553b91ddffa1915048118542833d49145c5e3de

---
+++ resources/bluespice.insertCategoryEditor.js
@@ -0,0 +1,13 @@
+$( document ).on( 'click', '#ca-insert_category, #bs-insert-category', function( e ) {
+	e.preventDefault();
+
+	mw.loader.using( 'ext.bluespice.insertcategory.editor.dialog.scripts' ).done( function() {
+		var windowManager = OO.ui.getWindowManager();
+		var dialog = new ext.InsertCategory.ui.dialog.CategoryEditor();
+
+		windowManager.addWindows( [ dialog ] );
+		windowManager.openWindow( dialog );
+	} );
+
+	return false;
+} );
