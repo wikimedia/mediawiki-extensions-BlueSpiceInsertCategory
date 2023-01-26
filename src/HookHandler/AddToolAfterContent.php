9a143c1ad64d63853764ae33847b521c4af58381
miriam schlindwein <schlindwein@hallowelt.com>
2023-01-26 12:02:22 +0100

    Add categories to new skin slot
    ERM27484
    
    [4.2.x]
    
    Change-Id: I4d36a072a27b263463628470233cd8fab46c2308
    (cherry picked from commit 07410bb819a3ebdd999b0bf13033079e1e87462e)

--- src/HookHandler/AddToolAfterContent.php
+++ src/HookHandler/AddToolAfterContent.php
@@ -11,10 +11,26 @@ class AddToolAfterContent implements MWStakeCommonUIRegisterSkinSlotComponents {
 	 * @inheritDoc
 	 */
 	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		/**
+		 * NOTE
+		 * With 4.2.5 SkinSlot ToolsAfterContent is not used anymore
+		 * Should be deleted with next minor
+		 */
 		$registry->register(
 			'ToolsAfterContent',
 			[
 				'insert-category' => [
+					'factory' => static function () {
+						return new InsertCategoryTool();
+					}
+				]
+			]
+		);
+
+		$registry->register(
+			'PageItems',
+			[
+				'categories' => [
 					'factory' => static function () {
 						return new InsertCategoryTool();
 					}
