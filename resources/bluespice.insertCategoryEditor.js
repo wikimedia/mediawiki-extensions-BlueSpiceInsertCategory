d14534c61db35ddcccd4dd1a929751252ae0aa0d
miriam schlindwein <schlindwein@hallowelt.com>
2023-12-15 11:02:23 +0100

    Fix key event on disabled button
    ERM35315
    
    [4.4.x]
    
    Change-Id: I2317fbc1b7813037786c8fffc4b26195fffc88c3

--- resources/bluespice.insertCategoryEditor.js
+++ resources/bluespice.insertCategoryEditor.js
@@ -1,6 +1,8 @@
 $( document ).on( 'click', '#ca-insertcategory, #bs-insert-category, #bs-category-link-edit', function( e ) {
 	e.preventDefault();
-
+	if ( $( e.currentTarget ).hasClass( 'isDisabled') ) {
+		return false;
+	}
 	mw.loader.using( 'ext.bluespice.insertcategory.editor.dialog.scripts' ).done( function() {
 		var windowManager = OO.ui.getWindowManager();
 		var dialog = new ext.InsertCategory.ui.dialog.CategoryEditor();
