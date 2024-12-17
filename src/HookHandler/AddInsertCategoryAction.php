c2e2793d74b1878e1ffc4ca65bba8510628231d3
Mschlindwein <schlindwein@hallowelt.com>
2024-12-17 11:31:16 +0100

    Hide category tool if content is not wikitext
    ERM39857
    
    [5.x]
    
    Change-Id: I0243d50184f22380ecf2e0e5f1a8e2e6659a81d5

--- src/HookHandler/AddInsertCategoryAction.php
+++ src/HookHandler/AddInsertCategoryAction.php
@@ -2,11 +2,29 @@
 
 namespace BlueSpice\InsertCategory\HookHandler;
 
+use MediaWiki\Content\WikitextContent;
 use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Page\WikiPageFactory;
+use MediaWiki\Permissions\PermissionManager;
 use SkinTemplate;
 
 class AddInsertCategoryAction implements SkinTemplateNavigation__UniversalHook {
+
+	/** @var PermissionManager */
+	private $permissionManager;
+
+	/** @var WikiPageFactory */
+	private $wikiPageFactory;
+
+	/**
+	 *
+	 * @param PermissionManager $permissionManager
+	 * @param WikiPageFactory $wikiPageFactory
+	 */
+	public function __construct( PermissionManager $permissionManager, WikiPageFactory $wikiPageFactory ) {
+		$this->permissionManager = $permissionManager;
+		$this->wikiPageFactory = $wikiPageFactory;
+	}
 
 	/**
 	 * @param SkinTemplate $sktemplate
@@ -16,13 +34,18 @@ class AddInsertCategoryAction implements SkinTemplateNavigation__UniversalHook {
 		if ( $sktemplate->getRequest()->getVal( 'action', 'view' ) != 'view' ) {
 			return true;
 		}
-		if ( !MediaWikiServices::getInstance()->getPermissionManager()
-			->userCan(
+		$title = $sktemplate->getTitle();
+		if ( !$this->permissionManager->userCan(
 				'edit',
 				$sktemplate->getUser(),
-				$sktemplate->getTitle()
+				$title
 			)
 		) {
+			return true;
+		}
+		$wikipage = $this->wikiPageFactory->newFromTitle( $title );
+		$content = $wikipage->getContent();
+		if ( !$content instanceof WikitextContent ) {
 			return true;
 		}
 		return false;
