f988b4ea632dbb0778065b05c27777d4dfdbf703
hslater <hamish.slater@hallowelt.com>
2025-04-17 21:11:51 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I04073bb1ef69615d5a47e05aa6188b19104bdbbb
    (cherry picked from commit 74d46b17d9650ae99db71858dc4d25e3a319d1d8)

--- resources/bluespice.insertCategoryEditor.js
+++ resources/bluespice.insertCategoryEditor.js
@@ -1,11 +1,11 @@
-$( document ).on( 'click', '#ca-insertcategory, #bs-insert-category, #bs-category-link-edit', function( e ) {
+$( document ).on( 'click', '#ca-insertcategory, #bs-insert-category, #bs-category-link-edit', ( e ) => {
 	e.preventDefault();
-	if ( $( e.currentTarget ).hasClass( 'isDisabled') ) {
+	if ( $( e.currentTarget ).hasClass( 'isDisabled' ) ) {
 		return false;
 	}
-	mw.loader.using( 'ext.bluespice.insertcategory.editor.dialog.scripts' ).done( function() {
-		var windowManager = OO.ui.getWindowManager();
-		var dialog = new ext.InsertCategory.ui.dialog.CategoryEditor();
+	mw.loader.using( 'ext.bluespice.insertcategory.editor.dialog.scripts' ).done( () => {
+		const windowManager = OO.ui.getWindowManager();
+		const dialog = new ext.InsertCategory.ui.dialog.CategoryEditor();
 
 		windowManager.addWindows( [ dialog ] );
 		windowManager.openWindow( dialog );
