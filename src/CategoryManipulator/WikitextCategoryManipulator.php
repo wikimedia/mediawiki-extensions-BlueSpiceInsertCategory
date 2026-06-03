ab67c4c1f30af82c4c4f623fbe48d8785145a47b
d.savuljesku <d.savuljesku@gmail.com>
2026-06-03 09:59:57 +0200

    Make sure to check if Title is valid before calling operations on it
    [5.1.x]
    
    ERM47961
    
    Change-Id: Ib086e2e317e06e8699a57473a1d1b128fbb78e32

--- src/CategoryManipulator/WikitextCategoryManipulator.php
+++ src/CategoryManipulator/WikitextCategoryManipulator.php
@@ -164,7 +164,7 @@ class WikitextCategoryManipulator implements ICategoryManipulator, LoggerAwareIn
 		preg_match_all( $pattern, $wikitext, $matches );
 		foreach ( $matches[2] as $index => $categoryName ) {
 			$title = $this->titleFactory->newFromText( $categoryName );
-			if ( $title->getNamespace() !== NS_CATEGORY ) {
+			if ( !$title || $title->getNamespace() !== NS_CATEGORY ) {
 				continue;
 			}
 			if ( !isset( $categories[$title->getText()] ) ) {
